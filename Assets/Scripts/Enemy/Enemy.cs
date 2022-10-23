9846014df01f6708cea8a73525893f2760ddc882
huanhoangvb <huanhoangvb@gmail.com>
2022-10-23 19:35:28 +0100

    add Gun and killable Enemy script

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDamage(float damage) {
+        health -= damage;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die() {
+        Destroy(gameObject);
+    }
+}
