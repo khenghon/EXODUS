8bcb1a8156e57411127e423cd1de4cc1254c731f
huanhoangvb <huanhoangvb@gmail.com>
2022-10-23 23:28:02 +0100

    add Particle Effect when gun hit object

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -7,7 +7,12 @@ public class Gun : MonoBehaviour
     public float damage = 10f;
     public float range = 100f;
 
+    public float impactForce = 30f;
+
+
     public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
 
     // Update is called once per frame
     void Update()
@@ -19,6 +24,7 @@ public class Gun : MonoBehaviour
     }
 
     void Shoot() {
+        muzzleFlash.Play();
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) {
             Debug.Log(hit.transform.name);
@@ -27,6 +33,13 @@ public class Gun : MonoBehaviour
             if (target != null) {
                 target.TakeDamage(damage);
             }
+
+            if (hit.rigidbody != null) {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 0.5f);
         }
     }
 }
