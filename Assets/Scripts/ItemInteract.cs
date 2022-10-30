9b500e17c60e077fa52a33d379202f721d03735c
huanhoangvb <huanhoangvb@gmail.com>
2022-10-30 22:13:07 +0000

    Item Inspection

---
+++ Assets/Scripts/ItemInteract.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class ItemInteract : ScriptableObject
+{
+    public Transform prefab;
+    public string prefabName;
+}
