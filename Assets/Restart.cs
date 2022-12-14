1da360e9ca4032b43a6bfb84bb3b6f7cd876f54b
afaqshafiq1974 <78801249+knewbeii@users.noreply.github.com>
2022-12-14 23:00:46 +0000

    try

--- Assets/Restart.cs
+++ Assets/Restart.cs
@@ -5,8 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class Restart : MonoBehaviour
 {
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+            RestartGame();
+        {
+            //if (GameIsPaused)
+            //{
+                
+            //    Resume();
+            //}else
+            //{
+            //    Pause();
+            //}
+        }
+    }
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
+    //void Resume()
+    //{
+    //    pauseMenuUI.SetActive(false);
+    //    Time.timeScale = 1f;
+    //    GameIsPaused = false;
+    //}
+    //void Pause()
+    //{
+    //    pauseMenuUI.SetActive(true);
+    //    Time.timeScale = 0f;
+    //    GameIsPaused = true;
+    //}
+
 }
