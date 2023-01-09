53bb3e75964f5b182ea789d3b689956050c4045e
peterakm <peterakm.23@gmail.com>
2023-01-09 04:38:23 +0000

    Working Enemies

---
+++ Assets/Animation/MonsterScriptAnimation/ChaseState.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class ChaseState : StateMachineBehaviour
+{
+    NavMeshAgent agent;
+    Transform player;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       player = GameObject.FindGameObjectWithTag("Player").transform;
+       NavMeshAgent agent = animator.GetComponent<NavMeshAgent>();
+       agent.enabled = false;
+
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent.SetDestination(animator.transform.position);
+        agent.enabled = true;
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Implement code that processes and affects root motion
+    }
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Implement code that sets up animation IK (inverse kinematics)
+    }
+}
